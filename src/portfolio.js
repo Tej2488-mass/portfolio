b05ca29eb4f196163640c8e53bfa68bbf13d8068
Varney Massaley <massaleyvarney@gmail.com>
2025-07-14 01:41:25 +0000

    major updates like adding blog, and changing footer

--- src/portfolio.js
+++ src/portfolio.js
@@ -311,6 +311,7 @@ const blogSection = {
     "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
   displayMediumBlogs: true, // Set true to display fetched medium blogs instead of hardcoded ones
   blogs: [
+    /*
     {
       url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
       title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
@@ -323,8 +324,9 @@ const blogSection = {
       description:
         "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
     }
+    */
   ],
-  display: false // Set false to hide this section, defaults to true
+  display: true // Set false to hide this section, defaults to true
 };
 
 // Talks Sections
